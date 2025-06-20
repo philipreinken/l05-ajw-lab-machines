ba5e11562a181d638cdf36ffbb76b60794c20450
Philip Reinken <p.reinken@mailbox.org>
2025-06-20 09:40:18 +0200

    fix: use active arp-scan to find target hosts on the network

--- inventory.sh
+++ inventory.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+function arp_scan() {
+  if [ ! -f ".arp-cache" ]; then
+    echo "Running arp-scan to populate .arp-cache file..." >&2
+    sudo arp-scan --localnet | awk '/^[0-9]/ {print $1, $2}' | uniq | sort -u | tee .arp-cache
+  else
+    echo ".arp-cache file already exists, skipping arp-scan." >&2
+  fi
+
+  cat .arp-cache
+}
+
 function ip_for_mac() {
   local mac="$1"
   local ip
@@ -10,11 +21,11 @@ function ip_for_mac() {
     return 1
   fi
 
-  ip=$(ip neigh | grep "$mac" | awk '{print $1}')
+  ip=$(arp_scan | grep "$mac" | awk '{print $1}')
 
   if [[ -z "$ip" ]]; then
     echo "No IP found for MAC address $mac" >&2
-    return 1
+    return
   fi
 
   echo "$ip"
@@ -23,10 +34,18 @@ function ip_for_mac() {
 function lab_machines() {
   while read -r -a line; do
     echo "Processing MAC: ${line[0]}, Hostname: ${line[1]}" >&2
+
+    local host_ip; host_ip=$(ip_for_mac "${line[0]}")
+
+    if [ -z "$host_ip" ]; then
+      echo "Skipping ${line[1]} due to missing IP for MAC ${line[0]}" >&2
+      continue
+    fi
+
     jq -n \
       --arg mac "${line[0]}" \
       --arg hostname "${line[1]}" \
-      --arg ip "$(ip_for_mac "${line[0]}")" \
+      --arg ip "${host_ip}" \
       '{ ($hostname): { ansible_user: "schule", ansible_host: $ip, security_ssh_allowed_users: [ "schule", "L05" ] } }'
   done <inventory-macs.txt | jq -s
 }
